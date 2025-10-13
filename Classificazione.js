fa30cf1a105038f0bf65659bfbcb512654c29419
francescanovelli <novellifrancescaa@gmail.com>
2025-10-13 16:49:46 +0200

    Create Classificazione.js

---
+++ Classificazione.js
@@ -0,0 +1,27 @@
+// Creo i poligoni per una classificazione supervisionata
+var polygons = ee.FeatureCollection([
+ ee.Feature(Forest, {'class': 0}),
+ ee.Feature(nonForest, {'class': 1})
+]);
+print(polygons);
+Map.addLayer(polygons);
+
+// Calcolo il BAP
+var library = require("users/sfrancini/bap:library"); 
+var BAPCS = library.BAP(null, "08-01", 30, 70, 0.7, 0.2, 0.3, 1500); // ingoro gli ultimi 4 parametri, avrò una collazione di immagini
+var predictors = BAPCS.filter(ee.Filter.calendarRange(2022, 2022, "year")).first(); // filtro per creare un immagine di un solo anno
+print(predictors);
+
+// Uso queste bande per la predizzione
+var bands = ["blue", "green", "red", "nir", "swir1", "swir2"];
+
+// Dataset di training, questo associa a tutti i pixel dei valori
+var training = predictors.sampleRegions({
+collection: polygons, // Questo da i valori ad ogni poligono
+properties: ['class'],
+// Set the scale to get Landsat pixels in the polygons.
+scale: 30
+});
+
+print(training.first());
+print(training.size());
