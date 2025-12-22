99b742b7dfee3e28fae7b9365b4328767bf8c429
francescanovelli <novellifrancescaa@gmail.com>
2025-12-22 09:39:32 +0100

    Create Grafici_CFI.R

---
+++ Esame/Grafici_CFI.R
@@ -0,0 +1,30 @@
+library(viridis)
+
+CFI <- read.csv("classified_data_def.csv")
+
+CFI$sum <- rowSums(CFI[, c(4, 5, 6, 7, 8)])
+CFI$X0 <- round(((CFI$X0/CFI$sum)*100), 0)
+CFI$X1 <- round(((CFI$X1/CFI$sum)*100), 0)
+CFI$X2 <- round(((CFI$X2/CFI$sum)*100), 0)
+CFI$X3 <- round(((CFI$X3/CFI$sum)*100), 0)
+CFI$X4 <- round(((CFI$X4/CFI$sum)*100), 0)
+
+ggplot(data = CFI, aes(x = year)) +
+  geom_smooth(aes(y = X0, colour = "Altro"), method = "loess") +
+  geom_smooth(aes(y = X1, colour = "Conifere"), method = "loess") + 
+  geom_smooth(aes(y = X2, colour = "Latifoglie"), method = "loess") +
+  geom_smooth(aes(y = X3, colour = "Latifoglie Sempreverdi"), method = "loess") +  
+  geom_smooth(aes(y = X4, colour = "Suolo Nudo"), method = "loess") +
+  labs(title = "Tipologia di Vegetazione",
+       x = "Anno",
+       y = "Percentuale (%)",
+       color = "Legenda") +
+  scale_color_manual(values = c("Altro" = "grey", 
+                                "Conifere" = "darkgreen", 
+                                "Latifoglie" = "yellow", 
+                                "Latifoglie Sempreverdi" = "lightgreen", 
+                                "Suolo Nudo" = "brown")) +
+  theme_light()
+
+valli <- unique(CFI$Nome.Valle)
+dv1 <- CFI[CFI$Nome.Valle == valli[1],]
